368d23a70245368c3aaf9e3ef398892f0a910d2f
KURAPIKA FK <smithashleydump123@gmail.com>
2026-03-24 03:07:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.post('/generate-brat', (req, res) => {
     const { text } = req.body;
 
     if (!text) {
-        return res.status(400).json({ error: 'Walang text na nilagay.' });
+        return res.status(400).json({ error: '.' });
     }
 
     const bratText = text.toLowerCase().trim();
